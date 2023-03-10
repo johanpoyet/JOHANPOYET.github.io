7d2a91283ab5ecf084a2875c7f6c50b1b0f5154c
johanpoyet <94459049+johanpoyet@users.noreply.github.com>
2023-03-10 18:41:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+    const scrollTop = window.pageTOffset ||
+        this.document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top = "-50px";
+    } else {
+        navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop;
+})
+
+
+
+// TYPED
+var typed = new Typed('.typed', {
+    strings: ["Je m'appelle <strong>Johan Poyet</strong > ", "Je suis actuellement en deuxième année de <strong>BTS SIO</strong> (Services Informatiques aux Organisations) option <strong>SLAM</strong> (Solutions Logicielles et Applications Métiers) au Lycée ICOF à Lyon. Dans ce portfolio vous découvrirez l'ensemble de mes expériences et projets en cours ainsi que pendant mes stages."],
+    typeSpeed: 20,
+});
+
+
+
+
+
+// COMPTEUR LIVE
+
+
+let compteur = 0;
+
+$(window).scroll(function () {
+    const top = $('.counter').offset().top - window.innerHeight;
+
+    if (compteur == 0 && $(window).scrollTop() > top) {
+        $('.counter-value').each(function () {
+            let $this = $(this),
+                countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+                {
+                    duration: 10000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    }
+                });
+        });
+        compteur = 1;
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    el_autohide = document.querySelector('.autohide');
+
+    // add padding-top to bady (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+
+    if (el_autohide) {
+        var last_scroll_top = 0;
+        window.addEventListener('scroll', function () {
+            let scroll_top = window.scrollY;
+            if (scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+        });
+        // window.addEventListener
+    }
+    // if
+
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
